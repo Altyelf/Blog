2769b7302980f505245a5a861e0e64871ccc3ef3
Altyelf <ilze.savicka211@gmail.com>
2020-11-26 16:07:08 +0200

    update

--- src/components/posts/posts.tsx
+++ src/components/posts/posts.tsx
@@ -1,16 +1,21 @@
-import React, {useEffect} from 'react';
+import React, {FC} from 'react';
 import './posts';
+import { Posts } from './posts-list'
+import { postsList } from './posts-list'
 
+type Props = {
+    id: string;
+    title: string;
+    image: string;
+  };
 
-
-
-const Posts = () => {
+  const Posts: FC<Props> = ({ id,  title, image }) => {
 
     return (
         <section className="posts-wrapper">
-            <h1 className="posts-heading">title</h1>
+            <h1 className="posts-heading">{title}</h1>
             <div className="posts-image">
-                <img src="" alt=""/>
+                <img src={image} alt="No image"/>
             </div>
             <button className="posts-button">Read more</button>
         </section>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useParams, useHistory } from 'react-router-dom';
 
 const Home = () => {
 
@@ -9,6 +11,11 @@ const Home = () => {
                     <div className="col-xs-12">
                         <h1 className='heading'>Pusheens thoughts</h1>
                     </div>                   
+                </div>
+                <div className="row">
+                    <div className="col-xs-4">
+
+                    </div>
                 </div>
             </div>
         </section>
